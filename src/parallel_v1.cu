af852f3efa93ef9241b37280f53562a869ccb2f3
lonk10 <luca0098@gmail.com>
2024-08-24 20:54:08 +0200

    fixed undefined behaviour tied to CSC/CSR not being in device memory

--- src/parallel_v1.cu
+++ src/parallel_v1.cu
@@ -43,14 +43,18 @@ __global__ void assignToParts(int n, int node, int *parts, float *result, int *i
     int r_size = end_r - start_r;
     int c_size = end_c - start_c;
     int max_size = r_size > c_size ? r_size : c_size;
+    /*
     if (ind == 0){
         cudaMalloc( (void**)&sdata_i, max_size * sizeof(int));
         cudaMalloc( (void**)&sdata_e, max_size * sizeof(int));
     }
-    __syncthreads(); // wait for allocation
+    __syncthreads(); // wait for allocation*/
     // initialization
     sdata_i[ind] = 0;
     sdata_e[ind] = 0;
+    __syncthreads();
+    if (ind == 0)
+        printf("Hello, thread %d of block %d init done\n", ind, k);
     __syncthreads();
 
     // gather values
@@ -63,7 +67,7 @@ __global__ void assignToParts(int n, int node, int *parts, float *result, int *i
             sdata_e[ind] = csr_rep -> values[start_r + ind];
         }
     }
-    if (ind < r_size){
+    if (ind < c_size){
         edge_node = csc_rep -> row_indexes[start_c + ind];
         if (parts[k*n+edge_node]){
             sdata_i[ind] = csc_rep -> values[start_c + ind];
@@ -72,46 +76,95 @@ __global__ void assignToParts(int n, int node, int *parts, float *result, int *i
         }
     }
     __syncthreads();
+    if (ind == 0)
+        printf("Hello, thread %d of block %d gathered values\n", ind, k);
+    __syncthreads();
+    
 
     // reduction
     for (int stride = blockDim.x / 2; stride > 0; stride >>= 1){
-        if (ind < stride){
+        if (ind < stride && (ind + stride) < max_size){
             sdata_i[ind] += sdata_i[ind + stride];
             sdata_e[ind] += sdata_e[ind + stride];
         }
         __syncthreads();
     }
+    if (ind == 0){
+        printf("Hello, thread %d of block %d reduction done\n", ind, k);
+    }
 
     // store final result
     if (ind == 0){
         int mu_k = 2*(int_costs[k] + sdata_i[0]);
-        result[k] = 100*(mu_k / (mu_k + ext_costs[k] + sdata_e[0]));
+        result[k] = 100*((float) mu_k / (float)(mu_k + ext_costs[k] + sdata_e[0]));
+        printf("mu_k: %d idata: %d edata: %d result: %f \n", mu_k, sdata_i[0], sdata_e[0], result[k]);
     }
+    __syncthreads();
 }
 
-__host__ void repair(int *parts, int k, int *destr_mask, int n, int m, int *int_costs, int *ext_costs, CSR *csr_rep, CSC *csc_rep){
+__global__ void assignToBestPart(int k, float *results, int n, int node, int *parts){
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    extern __shared__ int sdata[];
+    if (tid == 0){
+        cudaMalloc( (void**)&sdata, k * sizeof(int));
+    }
+    __syncthreads();
+    if (tid < k){
+        sdata[tid] = tid;
+        __syncthreads();
+        int nextTid;
+        for (int stride = blockDim.x / 2; stride > 0; stride >>= 1){
+            nextTid = sdata[tid + stride];
+            if (tid < stride){
+                if (results[tid] < results[tid + stride])
+                    sdata[tid] = nextTid;
+            }
+            __syncthreads();
+        }
+        if (tid == 0){
+            parts[sdata[0]*n+node] = 1;
+            printf("Assigned node %d to part %d\n", node, sdata[0]);
+        }
+    }
+
+}
+
+void repair(int *parts, int k, int *destr_mask, int n, int m, int *int_costs, int *ext_costs, CSR *csr_rep, CSC *csc_rep){
     //int i = 0;
     int node;
-    float *result;
-    cudaMalloc( (void**)&result, k * sizeof(float));
+    float *d_result;
+    cudaMalloc( (void**)&d_result, k * sizeof(float));
+    float *result = (float *) malloc(k*sizeof(float));
     int asgn;
-    int temp_cost;
+    float temp_cost;
     for (int i = 0; i < (n*m/100); i++){
         //k = asgn_mask[i];
         node = destr_mask[i];
-        assignToParts<<<k, THREADS_PER_BLOCK>>>(n, node, parts, result, int_costs, ext_costs, csr_rep, csc_rep);
+        assignToParts<<<k, THREADS_PER_BLOCK, 2 * n*n * sizeof(int)>>>(n, node, parts, d_result, int_costs, ext_costs, csr_rep, csc_rep);
+
+        //debug stuff
+        cudaMemcpy(result, d_result, k*sizeof(float), cudaMemcpyDeviceToHost);
+        for (int z = 0; z < k; z++){
+            printf("result[%d]: %d\n", z, result[z]);
+        }
         cudaDeviceSynchronize();
+        assignToBestPart<<<k, THREADS_PER_BLOCK>>>(k, d_result, n, node, parts);
+        cudaDeviceSynchronize();
+        /*
+        cudaMemcpy(d_result, result, k*sizeof(float), cudaMemcpyDeviceToHost);
         asgn = 0;
         temp_cost = result[0];
         for (int j = 0; j < k; j++){
             if (result[j] > temp_cost){
                 asgn = j;
                 temp_cost = result[j];
+                printf("new best result is result is %f\n", result[j]);
             }
         }
         
         //actual assign
-        parts[asgn*n+node] == 1;
+        parts[asgn*n+node] = 1;
+        printf("assigned node %d to part %d\n", node, asgn);*/
     }
     cudaFree(result);
 }
@@ -129,12 +182,14 @@ void lns_v1(int *in_parts, int *weights, int parts_num, int nodes_num, int edges
         best[i] = in_parts[i];
     }
     //compute node costs
-    int *temp_int_cost, *temp_ext_cost;
+    int *d_temp_int_cost, *d_temp_ext_cost;
 
     int *int_cost = (int *)malloc(parts_num*sizeof(int));
     int *ext_cost = (int *)malloc(parts_num*sizeof(int));
-    cudaMalloc( (void**)&temp_int_cost, parts_num * sizeof(int));
-    cudaMalloc( (void**)&temp_ext_cost, parts_num * sizeof(int));
+    int *temp_int_cost = (int *)malloc(parts_num*sizeof(int));
+    int *temp_ext_cost = (int *)malloc(parts_num*sizeof(int));
+    cudaMalloc( (void**)&d_temp_int_cost, parts_num * sizeof(int));
+    cudaMalloc( (void**)&d_temp_ext_cost, parts_num * sizeof(int));
     computeAllEdgeCost(best, row_rep, col_rep, parts_num, nodes_num, edges_num, int_cost, ext_cost);
     for (int i = 0; i < parts_num; i++){
         printf("init node cost %d \ninit edge cost %d\n", int_cost[i], ext_cost[i]);
@@ -146,6 +201,39 @@ void lns_v1(int *in_parts, int *weights, int parts_num, int nodes_num, int edges
     int *destr_mask = (int *) malloc(destr_nodes * sizeof(int));
     cudaMalloc( (void**)&d_destr_mask, destr_nodes * sizeof(int));
     cudaMalloc( (void**)&temp, nodes_num * parts_num * sizeof(int));
+
+
+    // copy CSR / CSC to device
+    CSR *d_row_rep;
+    CSC *d_col_rep;
+    int *row_offsets, *col_offsets, *col_indexes, *row_indexes, *row_values, *col_values;
+    printf("Allocation d_row, d_col\n");
+    cudaMalloc( (void**)&d_row_rep, sizeof(CSR));
+    cudaMalloc( (void**)&row_offsets, (nodes_num + 1) * sizeof(int));
+    cudaMalloc( (void**)&col_indexes, edges_num * sizeof(int));
+    cudaMalloc( (void**)&row_values, edges_num * sizeof(int));
+    cudaMalloc( (void**)&d_col_rep, sizeof(CSC));
+    cudaMalloc( (void**)&col_offsets, (nodes_num + 1) * sizeof(int));
+    cudaMalloc( (void**)&row_indexes, edges_num * sizeof(int));
+    cudaMalloc( (void**)&col_values, edges_num * sizeof(int));
+    printf("Copying temps\n");
+    cudaMemcpy(&(d_row_rep->offsets), &row_offsets, sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_row_rep->col_indexes), &col_indexes, sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_row_rep->values), &row_values, sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_col_rep->offsets), &col_offsets, sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_col_rep->row_indexes), &row_indexes, sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_col_rep->values), &col_values, sizeof(int*), cudaMemcpyHostToDevice);
+    printf("Copying into temps\n");
+    cudaMemcpy(row_offsets, row_rep->offsets, (nodes_num + 1) * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(col_indexes, row_rep->col_indexes, edges_num * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(row_values, row_rep->col_indexes, edges_num * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(col_offsets, col_rep->offsets, (nodes_num + 1) * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(row_indexes, col_rep->row_indexes, edges_num * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(col_values, col_rep->values, edges_num * sizeof(int), cudaMemcpyHostToDevice);
+    
+
+
+
     srand(time(NULL));
 
     printf("Initial cost is: %f\n", best_cost);
@@ -158,29 +246,32 @@ void lns_v1(int *in_parts, int *weights, int parts_num, int nodes_num, int edges
         }
         //resetMask<<<parts_num, THREADS_PER_BLOCK>>>(destr_mask, destr_nodes);
         cudaMemcpy(temp, in_parts, nodes_num*parts_num*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(temp_int_cost, int_cost, parts_num*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(temp_ext_cost, ext_cost, parts_num*sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_temp_int_cost, int_cost, parts_num*sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_temp_ext_cost, ext_cost, parts_num*sizeof(int), cudaMemcpyHostToDevice);
 
         //printf("Destroy step %d\n", iter);
         //destroy step
         computeRandomMask(destr_mask, nodes_num, m);
         cudaMemcpy(d_destr_mask, destr_mask, destr_nodes*sizeof(int), cudaMemcpyHostToDevice);
-        destroy<<<parts_num, THREADS_PER_BLOCK>>>(temp, destr_mask, m);
+        destroy<<<parts_num, THREADS_PER_BLOCK>>>(temp, d_destr_mask, m);
         cudaDeviceSynchronize();
 
         //printf("Repair step %d\n", iter);
         //repair step
         //computeRandomAssignment(asgn_mask, nodes_num, m, parts_num);
-        repair(temp, parts_num, destr_mask, nodes_num, m, temp_int_cost, temp_ext_cost, row_rep, col_rep);
+        repair(temp, parts_num, destr_mask, nodes_num, m, d_temp_int_cost, d_temp_ext_cost, d_row_rep, d_col_rep);
 
         //printf("Accept step %d\n", iter);
         //accept step
-        if (checkMass(temp, weights, parts_num, nodes_num, max_mass)){
+        cudaMemcpy(temp_int_cost, d_temp_int_cost, parts_num*sizeof(int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(temp_ext_cost, d_temp_ext_cost, parts_num*sizeof(int), cudaMemcpyDeviceToHost);
+        if (checkMass(temp_int_cost, parts_num, max_mass)){
             new_cost = computeCost(temp_int_cost, temp_ext_cost, parts_num);
+            printf("New cost found is: %f\n", new_cost);
             if (new_cost > best_cost)
             printf("New best cost is: %f\n", new_cost);
                 best_cost = new_cost;
-                memcpy(best, temp, nodes_num*parts_num*sizeof(int));
+                cudaMemcpy(best, temp, nodes_num*parts_num*sizeof(int), cudaMemcpyDeviceToHost);
         }
     }
     printf("Final cost is: %f\n", best_cost);
