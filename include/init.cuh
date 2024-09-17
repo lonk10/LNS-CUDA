76498a5f75d87a1b132a4a164d5baf3478d1dd6b
lonk10 <luca0098@gmail.com>
2024-09-17 21:30:42 +0200

    fixed serial for high number of partitions

--- include/init.cuh
+++ include/init.cuh
@@ -1,7 +1,7 @@
 #ifndef INIT_H
 #define INIT_H
 
-void readInput(FILE *in_file, int *partitions, int *parts, int nodes_num, int edges_num, int parts_num, int *offsets, int *indexes, int *values);
+void readInput(FILE *in_file, int *parts, int nodes_num, int edges_num, int parts_num, int *offsets, int *indexes, int *values);
 void csrSetup(int nodes_num, int edges_num, int *mat, int *h_csr_offsets, int *h_csr_columns, int *h_csr_values);
 void cscSetup(int nodes_num, int edges_num, int *mat, int *h_csc_offsets, int *h_csc_columns, int *h_csc_values);
 void cusparseSetup(CSR *csr, CSC *csc, int n, int e);
